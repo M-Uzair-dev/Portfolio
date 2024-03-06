5262658c2096674c1ad6b2b318cb44cc264d5784
Uzair manan <uzairmanan3@gmail.com>
2024-03-06 11:22:21 -0800

    fixing locomotive

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,15 @@ import logo from "../images/logo.png";
 
 function App() {
   const locomotiveScroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+    el: document.querySelector(".smooth-scroll"),
     smooth: true,
-    lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
-    multiplier: 1.4, // Effect Multiplier
-    reloadOnContextChange: true,
-    touchMultiplier: 2,
-    smoothMobile: 0,
-    smartphone: {
-        smooth: !0,
-        breakpoint: 767
+    mobile: {
+      smooth: true,
     },
     tablet: {
-        smooth: !1,
-        breakpoint: 1024
+      smooth: true,
     },
-});
-
-setTimeout(() => {  
-    locomotiveScroll.destroy();
-}, 0);
-setTimeout(() => {  
-    locomotiveScroll.init();
-}, 50);
-setTimeout(() => {  
-    locomotiveScroll.update();
-}, 1000);
-
+  });
   return (
     <>
       <div className="mainframe">
