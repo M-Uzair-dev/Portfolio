90f5384f823be4552611f8af6cb72f065b0820d0
Uzair manan <uzairmanan3@gmail.com>
2024-03-14 12:14:50 -0700

    Fixed a minor issue with scrolling

--- src/pages/js/ProjectDetails.jsx
+++ src/pages/js/ProjectDetails.jsx
@@ -13,7 +13,7 @@ const ProjectDetails = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
     return () => {};
-  }, [projectName]);
+  }, []);
 
   const navigate = useNavigate();
   return (
